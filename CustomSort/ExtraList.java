5b5402938863452764e36a4fad3b556474148d7b
Carina Straub <carina.s@online.de>
2022-12-23 12:26:48 +0100

    Added comments and task 8

--- CustomSort/ExtraList.java
+++ CustomSort/ExtraList.java
@@ -1,8 +1,9 @@
 package CustomSort;
 public class ExtraList {
-    ListElement head;
-    ListElement tail;
+    ListElement head; // first element of the list
+    ListElement tail; // last element of the list
 
+    // List element, that can store an element and the next list element
     private class ListElement{
         public double element;
         public ListElement next;
@@ -12,16 +13,18 @@ public class ExtraList {
             this.next = next;
         }
     }
-    ExtraList(){
+    
+    // Constructors for ExtraList
+    ExtraList(){ // create empty list
         head = null;
         tail = null;
     }
 
-    ExtraList(double element){
+    ExtraList(double element){ // create list with one element
         head = new ListElement(element, null);
         tail = head;
     }
-    ExtraList(double[] array){
+    ExtraList(double[] array){ // create list with an array
         if (array == null) return;
         head = new ListElement(array[0], null);
         tail = head;
@@ -31,6 +34,7 @@ public class ExtraList {
         }
     }
 
+    // Add elements at the end of the list and update the tail
     public void add(double element){
         if (head == null) {
             head = new ListElement(element, null);
@@ -41,6 +45,7 @@ public class ExtraList {
         }
     }
 
+    // Append an other list at the end of this list and update tail
     public void append(ExtraList list){
         if (list.head == null) return;
         if (this.head == null) {
@@ -51,21 +56,25 @@ public class ExtraList {
         this.tail = list.tail;
     }
 
+    // Check if there are one or less elements in the list
     public boolean oneOrLessElements(){
         if (this.isEmpty()) return true;
         return this.head.equals(tail);
     }
 
+    // Check if the list is empty
     public boolean isEmpty(){
         return (head == null);
     }
 
+    // Removes the first element of the list and returns it
     public double getFirst(){
         double first = head.element;
         head = head.next;
         return first;
     }
 
+    // converts the list to a string
     public String toString(){
         String result = "[";
         ListElement currentElement = head;
@@ -74,6 +83,24 @@ public class ExtraList {
             currentElement = currentElement.next;
         }
         result = result + tail.element + "]";
+        return result;
+        
+    }
+
+    // converts the list to an array
+    public double[] toArray(){
+        int size = 0;
+        ListElement currentElement = head;
+        while (!(currentElement == null)){
+            size += 1;
+            currentElement = currentElement.next;
+        }
+        currentElement = head;
+        double[] result = new double[size];
+        for (int index = 0; index < result.length; index++) {
+            result[index] = currentElement.element;
+            currentElement = currentElement.next;
+        }
         return result;
         
     }
--- CustomSort/Sorting.java
+++ CustomSort/Sorting.java
@@ -4,37 +4,49 @@ import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Sorting {
+    // Takes two arguments: array size and type of sorting algorithem and prints a running time
     public static void main(String[] args) {
+        // If there are not two arguments, the code can't be excuted
         if (args.length != 2) {
             System.out.print(-1);
             return;
         }
+        // The first argument is the Arraysize.
         int sampleSize = Integer.parseInt(args[0]);
         long time = 0;
 
-        for (int index = 0; index < 100; index++) {
+        // Try the sorting algorithem 50. times to get a more accurate result.
+        for (int index = 0; index < 50; index++) {
+            // Create a random array of the given size.
             double[] array = ThreadLocalRandom.current().doubles(sampleSize).toArray();
 
+            // initialize start and end (retures -1 if values are not overwritten)
             long start = 0, end = -2;
             switch (args[1]) {
                 case "selection":
-                    start = System.nanoTime();
-                    selectionSort(array);
-                    end = System.nanoTime();
+                    start = System.nanoTime(); // start is the current time
+                    array = selectionSort(array); // sort the array
+                    end = System.nanoTime(); // end is the current time
                     break;
                 case "quick":
-                    ExtraList list = new ExtraList(array);
-                    start = System.nanoTime();
-                    list = quickSort(list);
-                    end = System.nanoTime();
+                    ExtraList list = new ExtraList(array); // make a new list from the array
+                    start = System.nanoTime(); // start is the current time
+                    list = quickSort(list); // sort the array
+                    end = System.nanoTime(); // end is the current time
+                    array = list.toArray(); // write the list in the array
                     break;
                 default:
                     break;
             }
+            // calculate the time
             time += end-start;
+            // run test and set time to -1 if they fail
+            if (!runTest(array)) time = -1;
         }
-        System.out.print(time/100.0);
-        /*
+        // print the average of the 50 run times
+        System.out.print(time/50.0);
+        
+        /* Some Tests:
          * double[] array = {7,5,8,1,9,6,7,4,10.5};
          * ExtraList list = new ExtraList(array);
          * 
@@ -89,5 +101,15 @@ public class Sorting {
         sortedList.append(equal);
         sortedList.append(quickSort(larger));
         return sortedList;
+    }
+
+    // test if the array is increasing
+    public static boolean runTest(double[] array){
+        if (array.length < 1) return false;
+        boolean result = true;
+        for (int index = 0; index < array.length-1; index++) {
+            result = result && (array[index] <= array[index+1]);
+        }
+        return result;
     }
 }
